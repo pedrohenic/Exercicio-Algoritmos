5a6c699aaa1aa509b003eaa03795ecf81281d3d9
Pedro Henrique <phenriquenicolini@gmail.com>
2026-04-13 15:57:41 -0300

    oi

--- Exercicio Algoritmos.cpp
+++ Exercicio Algoritmos.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <clocale>
 
 using namespace std;
 
-int main(){
-    cout << "olá mundo";
-    
-    return 0;   
+int main()
+{
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+
+    cout << "Olá Mundo!" << endl;
+    cout << "Bem-vindo ao exercício de algoritmos!" << endl;
+
+    return 0;
 }
